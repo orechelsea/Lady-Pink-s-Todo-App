4ed9c220da898f6f535d85111859776d0beaf027
Chelsea Osayomi <orechelsea04@gmail.com>
2024-07-29 19:13:32 +0000

    "connected my database and made sure sqlite is commented out and migrated the necessary changes"

--- ladypink/settings.py
+++ ladypink/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g4_qvs)o)%756&w+^7r$3yc(l!p$qsm6b8@l+r2h1xp@xtmm4$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['https://8000-orechelsea-ladypinkstod-n5m70bx4tdh.ws.codeinstitute-ide.net', ".herokuapp.com"]
 
@@ -77,13 +81,16 @@ WSGI_APPLICATION = 'ladypink.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#DATABASES = {
+    #'default': {
+       # 'ENGINE': 'django.db.backends.sqlite3',
+       # 'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
