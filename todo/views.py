acf5a6eec0cab1527073d8546c1995616214beef
Chelsea Osayomi <orechelsea04@gmail.com>
2024-07-29 12:27:43 +0000

    "changed my views to incorporate both my static message and the form at the same time"

--- todo/views.py
+++ todo/views.py
@@ -5,11 +5,19 @@ from .forms import MyForm
 
 # Create your views here.
 def index(request):
+    context = {
+        'greeting': 'Hello! and welcome to Lady Pink\'s Todo List App to keep you from pulling your hair out and get you organised!!! What are you waiting for? Sign Up Below to join thousands of Pinkies in the organisation frenzy!!.',
+    }
+
     if request.method == 'POST':
         form = MyForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('index')
+            form.save() 
+            return redirect('index')  
     else:
         form = MyForm()
-    return render(request, "index.html", {"form": form})
+
+   
+    context['form'] = form
+
+    return render(request, 'index.html', context)
